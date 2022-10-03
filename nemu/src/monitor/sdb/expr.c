cf3641abd0291bf3248cd9bec348c86e8202cdbe
tracer-ics2022 <tracer@njuics.org>
2022-10-04 01:27:58 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:27:58 up 21 min,  1 user,  load average: 0.41, 0.30, 0.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -86,8 +86,9 @@ static bool make_token(char *e) {
   regmatch_t pmatch;
 
   nr_token = 0;
-  	printf("%c",*(e+14));
+  
   while (e[position] != '\0') {
+  	printf("%c",*(e+14));
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
@@ -131,7 +132,7 @@ static bool make_token(char *e) {
 
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
-      return false;
+      //return false;
     }
   }
 
