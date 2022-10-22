3b06c050d76b6e4c0e41061ed3e5a9d78102c2dc
tracer-ics2022 <tracer@njuics.org>
2022-10-22 21:35:29 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:35:29 up 10:41,  1 user,  load average: 0.26, 0.32, 0.36

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -24,7 +24,7 @@
 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, TYPE_J, // none
+  TYPE_N, TYPE_J,TYPE_B,TYPE_R // none
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
@@ -32,7 +32,8 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immJ() do { *imm =SEXT(BITS(i,31,31)<<20 | BITS(i,30,21)<<1 | BITS(i,20,20)<<11 |BITS(i,19,12)<<12,20);} while(0)
+#define immJ() do { *imm =SEXT(BITS(i,31,31)<<20 | BITS(i,30,21)<<1 | BITS(i,20,20)<<11 |BITS(i,19,12)<<12,21);} while(0)
+#define immB() do { *imm = SEXT(BITS(i,31,31)<<12 | BITS(i,30,25)<<5 | BITS(i,11,8)<<1 | BITS(i,7,7)<<11,13);} while(0)
 
 static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -45,6 +46,8 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
 	case TYPE_J:                   immJ(); break;
+	case TYPE_B: src1R(); src2R(); immB(); break;
+	case TYPE_R: src1R(); src2R();         break;
   }
 }
 
@@ -68,7 +71,12 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(dest) = src1 + imm);
   INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(dest) = 4 + s->pc, s->dnpc = s->pc +imm);
   INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(dest) = s->pc +4; s->dnpc = (src1+imm)&~1 );
-
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, if(src1==src2) s->dnpc+=imm);
+  INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add    , R, R(dest)=src1+src2);
+  INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(dest)=src1-src2);
+  INSTPAT("??????? ????? ????? 011 ????? 00100 11", sltiu  , I, R(dest)=(src1<(uint32_t)imm));
+  INSTPAT("??????? ????? ????? 001 ????? 11000 11", bne    , B, if(src1!=src2) s->dnpc +=imm);
+  
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
