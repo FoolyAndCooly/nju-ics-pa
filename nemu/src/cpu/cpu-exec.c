7b96ced3c131986e23d5057e7ad167ef7a498814
tracer-ics2022 <tracer@njuics.org>
2022-10-22 21:56:58 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:56:58 up 11:02,  1 user,  load average: 1.28, 0.59, 0.38

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -22,7 +22,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INST_TO_PRINT 10
+#define MAX_INST_TO_PRINT 20
 
 
 CPU_state cpu = {};
--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -57,7 +57,7 @@ static int cmd_q(char *args) {
 static int cmd_si(char *args){
   if (args == NULL)
   { cpu_exec(1);}
-  else{ cpu_exec(atoi(args));printf("%d",atoi(args));}
+  else{ cpu_exec(atoi(args));}
   return 0;
 }
 
