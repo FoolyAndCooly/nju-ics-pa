b0556041694a0a6303b89be278bfd3208df502bb
tracer-ics2022 <tracer@njuics.org>
2022-10-23 10:39:52 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:39:52 up 18 min,  1 user,  load average: 0.28, 0.28, 0.12

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -32,7 +32,7 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immJ() do { *imm =SEXT(BITS(i,31,31)<<20 | BITS(i,30,21)<<1 | BITS(i,20,20)<<11 |BITS(i,19,12)<<12,21);} while(0)
+#define immJ() do { *imm = SEXT(BITS(i,31,31)<<20 | BITS(i,30,21)<<1 | BITS(i,20,20)<<11 |BITS(i,19,12)<<12,21);} while(0)
 #define immB() do { *imm = SEXT(BITS(i,31,31)<<12 | BITS(i,30,25)<<5 | BITS(i,11,8)<<1 | BITS(i,7,7)<<11,13);} while(0)
 
 static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type) {
