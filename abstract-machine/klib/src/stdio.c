f2f912fd30b467a237dc92f971a3015e7e81dd2f
tracer-ics2022 <tracer@njuics.org>
2022-10-24 20:02:34 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:02:34 up  3:21,  1 user,  load average: 0.30, 0.27, 0.29

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,6 +2,8 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
+#include "string.h"
+#include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
@@ -10,11 +12,40 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	int i;
+	char c;
+	char *s;
+	while(*fmt){
+	if(*fmt != '%'){*out++ = *fmt;}
+	else{
+	switch(*++fmt){
+	case 'c':
+	c=(char)va_arg(ap,int);
+	*out++ =c;
+	break;
+	case 'd':
+	i=va_arg(ap,int);
+	*out++ =i;
+	break;
+	case 's':
+	s=va_arg(ap,char *);
+	strcat(out,s);
+	out=out+strlen(s);
+	}
+	}
+	*out='\0';
+	}
+	panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list ap;
+	int ret = -1;
+	va_start(ap, fmt);
+	ret = vsprintf(out , fmt, ap);
+	va_end(ap);
+	return ret;
+	panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
