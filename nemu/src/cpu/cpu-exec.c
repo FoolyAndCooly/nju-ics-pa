adeb4161ed1d43e49066019e333618ad57350559
tracer-ics2022 <tracer@njuics.org>
2022-10-27 16:53:15 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:53:15 up  6:08,  1 user,  load average: 0.52, 0.51, 0.47

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -87,7 +87,7 @@ static void execute(uint64_t n) {
   *cnt=0;
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc,cnt);
-     if (!(*cnt%16)){
+     if (!((*cnt)%16)){
      cnt=cnt-15; 
      }
      else{
