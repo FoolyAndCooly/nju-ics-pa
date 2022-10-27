e6b680cfb487cfddad98c7a2de577f2a3c04e93f
tracer-ics2022 <tracer@njuics.org>
2022-10-27 16:50:22 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:50:22 up  6:05,  1 user,  load average: 0.92, 0.63, 0.51

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -57,7 +57,7 @@ static void exec_once(Decode *s, vaddr_t pc,int *cnt) {
   isa_exec_once(s);
   cpu.pc = s->dnpc;
 #ifdef CONFIG_ITRACE
-char *p=s->logbuf;
+  char*p =s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
   int ilen = s->snpc - s->pc;
   int i;
