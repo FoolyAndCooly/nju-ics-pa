fe0d671de9cbe88771b072b8ef92992ac80ce6cc
tracer-ics2022 <tracer@njuics.org>
2022-10-27 16:13:29 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:13:29 up  5:28,  1 user,  load average: 0.49, 0.44, 0.43

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -81,6 +81,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
   memcpy(q, s->logbuf, p-(s->logbuf)+10);
+  printf("ok\n");
   cnt++;
   if (cnt%16){
   q=q-15;
