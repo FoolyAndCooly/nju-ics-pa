ae733e6b273694d2bbc8a90024622e7cf92043c4
tracer-ics2022 <tracer@njuics.org>
2022-10-28 20:07:47 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:07:47 up  2:15,  1 user,  load average: 0.58, 0.42, 0.31

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -16,13 +16,20 @@
 #include <isa.h>
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
+const char *regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(cpu.pc != ref_r->pc){
 	printf("pc is wrong\nref pc : 0x%08x\ncpu pc : 0x%08x\n",ref_r->pc,cpu.pc );
 	for(int i=0;i<32;i++){
 	if (cpu.gpr[i] !=ref_r->gpr[i]){
-	printf("%d reg is wrong\nref reg :0x%08x\ncpu reg :0x%08x\n",i,ref_r->gpr[i],cpu.gpr[i]);
+	printf("%s reg is wrong\nref reg :0x%08x\ncpu reg :0x%08x\n",regs[i],ref_r->gpr[i],cpu.gpr[i]);
 	return false ;
 	}
 	}
