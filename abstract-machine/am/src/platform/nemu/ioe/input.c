3d1994fb98d6b6b0515886ad10de6b76d2d8effa
tracer-ics2022 <tracer@njuics.org>
2022-10-30 19:51:20 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:51:20 up  5:44,  1 user,  load average: 1.16, 0.56, 0.50

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,6 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = inb(KBD_ADDR+12);
+  kbd->keydown = inb(KBD_ADDR);
   kbd->keycode = inb(KBD_ADDR);
 }
