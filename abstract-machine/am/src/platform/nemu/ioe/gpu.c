1c77aa14c665f6bf498208371d48d90c41f48d01
tracer-ics2022 <tracer@njuics.org>
2022-11-05 21:54:18 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:54:18 up  9:50,  1 user,  load average: 0.59, 0.39, 0.30

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -22,7 +22,18 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  uint32_t val=inl(VGACTL_ADDR);
+int i, j, pi, pj;
+  uint32_t* pixels = ctl->pixels;
+  uint32_t wh_data = inl(VGACTL_ADDR);
+  uint32_t width = (wh_data >> 16) & 0xffff;
+  for (i = ctl->y, pi = 0; pi < ctl->h; ++ i, ++pi) {
+    for (j = ctl->x, pj = 0; pj < ctl->w; ++ j, ++pj) {
+      int fb_offset = (i * width + j) * 4;
+      int pixel_offset = pi* ctl->w + pj;
+      outl(FB_ADDR + fb_offset, *(pixels + pixel_offset));
+    }
+  }
+  /*uint32_t val=inl(VGACTL_ADDR);
   int i=ctl->x,j=ctl->y,w0=0,h0=0;
   uint32_t width=val>>16;
   for(;i<ctl->y + ctl->h;i++){
@@ -31,7 +42,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   	h0++;
   	}
   	w0++;
-  }
+  }*/
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
