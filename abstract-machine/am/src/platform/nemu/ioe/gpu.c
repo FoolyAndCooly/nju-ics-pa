0621942e3755c2eebb622a3da3a418ae7a04348b
tracer-ics2022 <tracer@njuics.org>
2022-11-05 22:21:24 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:21:24 up 10:17,  1 user,  load average: 0.23, 0.43, 0.40

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -22,12 +22,12 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int i, j, h0, w0;
+  int i=ctl->y, j=ctl->x, h0, w0;
   uint32_t* pixels = ctl->pixels;
   uint32_t wh_data = inl(VGACTL_ADDR);
   uint32_t width = wh_data >> 16;
-  for (i=ctl->y,h0=0; h0 < ctl->h; ++ i, ++h0) {
-    for (j=ctl->x,w0=0; w0 < ctl->w; ++ j, ++w0) {
+  for (h0=0; h0 < ctl->h; ++ i, ++h0) {
+    for (w0=0; w0 < ctl->w; ++ j, ++w0) {
       outl(FB_ADDR + (i * width + j) * 4, *(pixels + h0* ctl->w + w0));
     }
   }
