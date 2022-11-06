d0ecfc0e5ddfc68906976ec644b834ad58559a39
tracer-ics2022 <tracer@njuics.org>
2022-11-06 10:23:30 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:23:30 up 10:02,  1 user,  load average: 0.73, 0.50, 0.36

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,12 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-	int i;
+	/*int i;
 	int w=400;
 	int h=300;
 	uint32_t *fb=(uint32_t*)(uintptr_t)FB_ADDR;
 	for(i=0;i<w*h;i++)fb[i]=i;
-	outl(SYNC_ADDR,1);
+	outl(SYNC_ADDR,1);*/
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
