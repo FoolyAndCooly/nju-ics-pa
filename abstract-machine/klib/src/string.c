c6c606dae7679fa2f3e5dbf0a88bca537cf64e02
tracer-ics2022 <tracer@njuics.org>
2022-11-06 10:12:02 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:12:02 up  9:50,  1 user,  load average: 0.23, 0.29, 0.27

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -119,10 +119,9 @@ void *memcpy(void *out, const void *in, size_t n) {
 
 int memcmp(const void *s1, const void *s2, size_t n) {
 	
-	 while( n-- &&(*((char*)s1) ==*((char*)s2))){
+	 while( --n && (*((char*)s1) ==*((char*)s2))){
 	s1=(char*)s1+1;
-	s2=(char*)s2+1;
-	
+	s2=(char*)s2+1;	
 	}
 	return *((char*)s1)-*((char*)s2);
 
