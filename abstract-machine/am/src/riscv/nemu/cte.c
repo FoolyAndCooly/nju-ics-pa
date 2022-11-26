ba2e15c921aa12ab5622667802ec730af51429bb
tracer-ics2022 <tracer@njuics.org>
2022-11-26 21:06:45 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:06:45 up  4:24,  1 user,  load average: 0.48, 0.67, 0.62

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,6 +8,9 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case 0xb:
+      ev.event = EVENT_YIELD;
+      break;
       default: ev.event = EVENT_ERROR; break;
     }
 
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,8 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case EVENT_YIELD:
+    printf("traped\n");
     default: panic("Unhandled event ID = %d", e.event);
   }
 
