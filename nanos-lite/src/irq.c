ae4cba39d7933743b14816ff78fd1eecbafd51d9
tracer-ics2022 <tracer@njuics.org>
2022-11-26 21:07:22 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:07:22 up  4:25,  1 user,  load average: 0.64, 0.68, 0.63

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
-    printf("traped\n");
+    printf("trapped\n");
     default: panic("Unhandled event ID = %d", e.event);
   }
 
