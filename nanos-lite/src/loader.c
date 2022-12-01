98c58623e4e2361584515ae8bf0bb7e941439c64
tracer-ics2022 <tracer@njuics.org>
2022-12-01 18:47:22 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:47:22 up  3:47,  1 user,  load average: 0.49, 0.20, 0.19

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -20,11 +20,11 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(&phdr,phoff,sizeof(Elf_Phdr));
   if(phdr.p_type==PT_LOAD){
   ramdisk_read((void*)phdr.p_vaddr,phdr.p_offset,phdr.p_filesz);
-  memset((void*)phdr.p_vaddr,0,phdr.p_memsz - phdr.p_filesz);
+  memset((void*)phdr.p_vaddr+ehdr.e_entry,0,phdr.p_memsz - phdr.p_filesz);
   }
   }
-  printf("%x\n %x\n",ehdr.e_entry,(void*)phdr.p_vaddr);
-  return 0;//ehdr.e_entry;
+  //printf("%x\n %x\n",ehdr.e_entry,(void*)phdr.p_vaddr);
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
