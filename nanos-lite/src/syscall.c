c0721cd91824885dfa65f94c27587751b0819f87
tracer-ics2022 <tracer@njuics.org>
2022-12-03 15:37:02 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:37:02 up 48 min,  1 user,  load average: 0.38, 0.50, 0.47

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,18 +1,57 @@
 #include <common.h>
 #include "syscall.h"
-#define SYS_yield 1
-#define SYS_exit 0
+#ifndef __SYSCALL_H__
+#define __SYSCALL_H__
+
+enum {
+  SYS_exit,
+  SYS_yield,
+  SYS_open,
+  SYS_read,
+  SYS_write,
+  SYS_kill,
+  SYS_getpid,
+  SYS_close,
+  SYS_lseek,
+  SYS_brk,
+  SYS_fstat,
+  SYS_time,
+  SYS_signal,
+  SYS_execve,
+  SYS_fork,
+  SYS_link,
+  SYS_unlink,
+  SYS_wait,
+  SYS_times,
+  SYS_gettimeofday
+};
+
+#endif 
 void sys_yield(Context *c){
 	yield();
 	c->GPRx=0;
 #ifdef CONFIG_STRACE
-	printf("yield");
+	printf("yield\n");
 #endif
 }
 void sys_exit(Context* c){
 	halt(c->GPR2);
 #ifdef CONFIG_STRACE
-	printf("exit"));
+	printf("exit\n");
+#endif
+}
+void sys_write(Context* c){
+	int fd=c->GPR2;
+	void* buf=(void*)c->GPR3;
+	uint32_t count=c->GPR4;
+	if(fd==1 || fd==2){
+	for(int i=0;i<count;i++){
+	putch(*(char*)buf);(char*)buf++;
+	}
+	c->GPRx=count;
+	}
+#ifdef CONFIG_STRACE
+	printf("write\n");
 #endif
 }
 void do_syscall(Context *c) {
@@ -22,6 +61,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_yield:sys_yield(c);break;
     case SYS_exit:sys_exit(c);break;
+    case SYS_write:sys_write(c);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
