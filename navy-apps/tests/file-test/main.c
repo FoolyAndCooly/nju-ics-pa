d7fc702f7e7fc8127df03d0e7408b0b2322af00d
tracer-ics2022 <tracer@njuics.org>
2022-12-05 00:01:48 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:01:48 up 13 min,  1 user,  load average: 0.62, 0.56, 0.36

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -2,7 +2,8 @@
 #include <assert.h>
 
 int main() {
-  FILE *fp = fopen("/share/files/num", "r+");
+  //FILE *fp = fopen("/share/files/num", "r+");
+  fopen("/share/files/num", "r+");
   //printf("%p\n",fp);
   //assert(fp);
 
