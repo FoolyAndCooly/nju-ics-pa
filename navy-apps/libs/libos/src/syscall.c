f50d1c92e173214336f130d7576a0606afe9b698
tracer-ics2022 <tracer@njuics.org>
2022-12-04 22:08:45 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:08:45 up  2:44,  1 user,  load average: 0.56, 0.53, 0.50

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -57,7 +57,8 @@ void _exit(int status) {
 
 int _open(const char *path, int flags, mode_t mode) {
   //_exit(SYS_open);
-  return _syscall_(SYS_open,(intptr_t)path,(intptr_t)flags,(intptr_t)mode);
+  //return _syscall_(SYS_open,(intptr_t)path,(intptr_t)flags,(intptr_t)mode);
+  return _syscall_(SYS_open,(intptr_t)path,0,0);
 }
 
 int _write(int fd, void *buf, size_t count) {
