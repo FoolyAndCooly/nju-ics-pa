6997188945b48861b39c41587a1a968bea6249be
tracer-ics2022 <tracer@njuics.org>
2022-12-05 17:14:07 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:14:07 up  2:19,  1 user,  load average: 0.67, 0.63, 0.71

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -47,16 +47,16 @@ void sys_exit(Context* c){
 }
 void sys_write(Context* c){
 	int fd=c->GPR2;
-	void* buf=(void*)c->GPR3;
+	const void* buf=(void*)c->GPR3;
 	char* p=(char*)buf;
 	uint32_t count=c->GPR4;
-	if(fd==1 ){
+	if(fd==1 || fd==2){
 	for(int i=0;i<count;i++){
 	putch(*p);p++;
-	}}
-	//c->GPRx=count;
-
-	c->GPRx=fs_write((int)c->GPR2,(const void*)c->GPR3,(size_t)c->GPR4);
+	}
+	c->GPRx=count;}
+	else{
+	c->GPRx=fs_write((int)c->GPR2,(const void*)c->GPR3,(size_t)c->GPR4);}
 #ifdef STRACE
 	printf("write\n");
 #endif
