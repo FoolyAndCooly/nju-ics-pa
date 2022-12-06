5201317179ef550096d3ad23975b67529ddd42d2
tracer-ics2022 <tracer@njuics.org>
2022-12-06 19:35:48 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:35:48 up 37 min,  1 user,  load average: 0.37, 0.56, 0.54

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -24,7 +24,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  if (ev.keycode == AM_KEY_NONE) return 0;
+  if (ev.keycode == AM_KEY_NONE) {printf("miss\n");return 0;}
   printf("get it !\n");
   char buf0[16]={0};
   char* p=buf0;
