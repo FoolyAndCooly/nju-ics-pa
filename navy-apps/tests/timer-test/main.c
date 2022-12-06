35467dfb847e94908c9a1661dbf4bbd3dfdea120
tracer-ics2022 <tracer@njuics.org>
2022-12-06 14:17:23 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:17:23 up  1:10,  1 user,  load average: 0.34, 0.37, 0.41

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -6,10 +6,10 @@ int main(){
   struct timeval tv;
   while (1) {
     gettimeofday(&tv,NULL);
-    while( tv.tv_sec < sec) ;
+    //while( tv.tv_sec < sec) ;
     //rtc = io_read(AM_TIMER_RTC);
     //printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
-      printf("(%d seconds).\n", sec);
+      printf("(%d seconds).\n", tv.tv_sec);
     
     sec ++;
   }
