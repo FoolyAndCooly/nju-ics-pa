73077ae55900e057f3bbc8781c5948301928d008
tracer-ics2022 <tracer@njuics.org>
2022-12-06 13:56:00 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:56:00 up 49 min,  1 user,  load average: 0.54, 0.48, 0.49

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -99,7 +99,7 @@ void sys_gettimeofday(Context* c){
 	uint64_t us=io_read(AM_TIMER_UPTIME).us;
 	tv->tv_sec = us / 1000000;
 	tv->tv_usec = us % 1000000;
-	c->GPR2=0;
+	c->GPRx=0;
 }
 void do_syscall(Context *c) {
   uintptr_t a[4];
