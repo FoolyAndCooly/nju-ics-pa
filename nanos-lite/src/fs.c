1f31df8ad18e33f5c33ccbe351d9d9280d1bad50
tracer-ics2022 <tracer@njuics.org>
2022-12-06 18:40:51 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:40:51 up 16 min,  1 user,  load average: 0.69, 0.73, 0.46

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -16,7 +16,7 @@ typedef struct {
   size_t open_offset;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB,FD_EVENT};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENT,FD_FB};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -34,6 +34,8 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
   [FD_EVENT]  = {"/dev/event", 0, 0, events_read, invalid_write},
+  [FD_FB] = {"/dev/fb", 0, 0, invalid_read, invalid_write},
+  
 #include "files.h"
 };
 static int num=sizeof(file_table)/sizeof(Finfo);
