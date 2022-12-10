d9e35a13eae0c50a7678f14f8d5257e2ce916de1
tracer-ics2022 <tracer@njuics.org>
2022-12-10 20:11:47 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:11:47 up  4:34,  1 user,  load average: 0.09, 0.23, 0.24

--- navy-apps/tests/fixedptc-test/main.c
+++ navy-apps/tests/fixedptc-test/main.c
@@ -1,7 +1,7 @@
 #include"/home/liushao/ics2022/navy-apps/libs/libfixedptc/include/fixedptc.h"
 #include <stdio.h>
 int main(){
-fixedpt a = fixedpt_rconst(1.2);
+fixedpt a = fixedpt_rconst(-1.2);
 fixedpt b = fixedpt_fromint(10);
 int c = 0;
   c = fixedpt_toint(fixedpt_ceil(a));
