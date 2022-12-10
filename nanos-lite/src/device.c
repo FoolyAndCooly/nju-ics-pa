5ae263cac5c40bd3711bd99b195b2dbcfc9385b4
tracer-ics2022 <tracer@njuics.org>
2022-12-10 17:20:02 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:20:02 up  1:42,  1 user,  load average: 0.54, 0.60, 0.54

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -66,9 +66,9 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   //int* p=(int*)buf;
   //while(1){printf("%x\n",*p++);}
   int off=offset/4;
-  int x=off / t.width;
-  int y=off % t.width;
-  io_write(AM_GPU_FBDRAW,x,y,(void*)buf,len/4,1,0);
+  int x=off % t.width;
+  int y=off / t.width;
+  io_write(AM_GPU_FBDRAW,x,y,(void*)buf,len/4,1,true);
   //printf("write  ok!\n");
   return 0;
 }
