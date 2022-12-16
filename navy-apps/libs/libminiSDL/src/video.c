156f8c235d83370c38ef8598190c9acede2eeff0
tracer-ics2022 <tracer@njuics.org>
2022-12-16 23:19:22 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:19:22 up  2:13,  1 user,  load average: 0.40, 0.31, 0.26

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -28,8 +28,8 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 printf("into\n");
   uint32_t dx,dy,dw,dh;
   printf("%d\n",dstrect);
-  if(dstrect=NULL){dw=dst->w;dh=dst->h;dx=0;dy=0;printf("NULL branch\n");}
-  else{dx=dstrect->x;dy=dstrect->y;dw=dstrect->w;dh=dstrect->h;printf("normal branch\n");}
+  if(dstrect==NULL){dw=dst->w;dh=dst->h;dx=0;dy=0;}
+  else{dx=dstrect->x;dy=dstrect->y;dw=dstrect->w;dh=dstrect->h;}
   printf("goto\n");
   uint32_t *q;
   //printf("goto1\n");
