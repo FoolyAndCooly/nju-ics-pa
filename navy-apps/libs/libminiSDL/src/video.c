457690a95d42e5836fb6f024eda300af3f0b7a0d
tracer-ics2022 <tracer@njuics.org>
2022-12-17 20:43:13 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:43:13 up  6:31,  1 user,  load average: 0.47, 0.61, 0.54

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -13,7 +13,7 @@ printf("Bli\n");
   else{sh=srcrect->h;sw=srcrect->w;sx=srcrect->x;sy=srcrect->y;}
   printf("1\n");
   if(dstrect==NULL){dx=0;dy=0;}else{dx=dstrect->x;dy=dstrect->y;}
-  printf("%p\n",src->pixels);
+  printf("%p %d %d %d %d\n",src->pixels,sx,src->w,sy,sh);
   uint32_t *p,*q;
   for(int i=0;i<sh;i++){
   p=(uint32_t*)src->pixels+sx+src->w * (sy+i);
