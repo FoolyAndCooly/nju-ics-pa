c751c1ab252d6797b71ea153787ef7f0e1432cd2
tracer-ics2022 <tracer@njuics.org>
2022-12-19 21:33:48 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:33:48 up  6:00,  1 user,  load average: 0.62, 0.76, 0.72

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -4,6 +4,7 @@
 
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
+#include <assert.h>
 SDL_Surface* STBIMG_LoadFromMemory(const unsigned char*,int);
 
 SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
@@ -14,6 +15,7 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 
 SDL_Surface* IMG_Load(const char *filename) {
   FILE* fp=fopen(filename,"r+");
+  assert(fp);
   fseek(fp,0,SEEK_END);
   uint32_t size=ftell(fp);
   fseek(fp,0,SEEK_SET);
