491139ae79eb0c167efae006a8d3e6af83ac7ea1
tracer-ics2022 <tracer@njuics.org>
2022-12-19 21:27:20 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:27:20 up  5:54,  1 user,  load average: 0.66, 0.66, 0.67

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -33,9 +33,9 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, serial_write},
+  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
   [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write},
   [FD_EVENT]  = {"/dev/events", 0, 0, events_read, invalid_write},
   [FD_DISPINFO]  = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
