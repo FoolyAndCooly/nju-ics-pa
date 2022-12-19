b68c4ae3a5663879151d5163ee599c863f72c94f
tracer-ics2022 <tracer@njuics.org>
2022-12-19 19:02:14 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:02:14 up  3:29,  1 user,  load average: 0.78, 0.83, 0.71

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -13,23 +13,19 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
-}
-
-int SDL_WaitEvent(SDL_Event *event) {
   char* buf=(char*)malloc(sizeof(char)*10);
   char* p=buf+3;
-  while(1){
-  NDL_PollEvent(buf,16);
-  //printf("%s\n",p);
+  if(NDL_PollEvent(buf,16) == 0) return 0;
   if(*buf=='k' && *(buf+1)=='d'){
-  //printf("get %s\n",p);
   event->type=SDL_KEYDOWN;
   if(strcmp(p,"DOWN")) event->key.keysym.sym=SDLK_DOWN;
   if(strcmp(p,"UP")) event->key.keysym.sym=SDLK_UP;
-  break;
   }
-  }
+  return 1;
+}
+
+int SDL_WaitEvent(SDL_Event *event) {
+  while(!SDL_PollEvent(event)) ;
   return 1;
 }
 
