2ad0561dafa6c879739435196922bf2266b17bee
tracer-ics2022 <tracer@njuics.org>
2022-12-19 19:06:24 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:06:24 up  3:33,  1 user,  load average: 0.85, 0.94, 0.81

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -122,7 +122,7 @@ static void statistic() {
 
 void assert_fail_msg() {
   isa_reg_display();
-  iring_display();
+  IFDEF(CONFIG_ITRACE,iring_display());
   statistic();
 }
 
@@ -147,7 +147,7 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
     case NEMU_END: case NEMU_ABORT:
-    iring_display();
+    IFDEF(CONFIG_ITRACE,iring_display());
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
