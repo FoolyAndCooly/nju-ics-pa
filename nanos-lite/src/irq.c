ffb6941339168db1d93df48b64e6b63a1d6d5db9
tracer-ics2022 <tracer@njuics.org>
2023-01-06 00:18:54 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:18:54 up 30 min,  1 user,  load average: 0.64, 0.81, 0.69

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,7 @@ static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
     c->mepc+=4;
-    schedule(c);
+    c=schedule(c);
     printf("trapped\n");
     break;
     case EVENT_SYSCALL:
