f3e80735da9a2f2bd3cf775ecc7abae474a1e094
tracer-ics2022 <tracer@njuics.org>
2023-01-06 14:37:54 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:37:54 up  2:34,  1 user,  load average: 0.68, 0.55, 0.43

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -8,6 +8,7 @@ void call_main(uintptr_t *args) {
   //char *empty[] =  {NULL };
   int argc=*args;
   char** argv=args+1;
+  printf("%s\n",argv[0]);
   char** envp=args+argc+2;
   environ = envp;
   exit(main(argc, argv, envp));
