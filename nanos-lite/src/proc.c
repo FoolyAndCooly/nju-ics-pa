1822cb88f78a2e5d525dc31f4f8645303ef93c62
tracer-ics2022 <tracer@njuics.org>
2023-01-06 13:17:12 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:17:12 up  1:13,  1 user,  load average: 0.65, 0.76, 0.58

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,11 +22,11 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, NULL);
-  context_uload(&pcb[1], "/bin/pal");
+  //context_kload(&pcb[0], hello_fun, NULL);
+  //context_uload(&pcb[1], "/bin/pal");
   switch_boot_pcb();
-  //char* argv[2]={"/bin/exec-test",NULL};
-  //printf("%s\n",argv[0]);
+  char* argv[2]={"/bin/exec-test",NULL};
+  printf("%s\n",argv[0]);
   /*const char* filename="/bin/nterm";
   naive_uload(NULL, filename);*/
   Log("Initializing processes...");
