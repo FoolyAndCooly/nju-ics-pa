89a48248a0e5f3bcf528cad424e9679627f1f3ac
tracer-ics2022 <tracer@njuics.org>
2023-01-06 14:49:54 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:49:54 up  2:46,  1 user,  load average: 0.50, 0.60, 0.53

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -67,7 +67,7 @@ void context_uload(PCB* pcb ,const char* filename,char* const argv[],char* const
   p-=strlen(argv[i])+1;
   strcpy(p,argv[i]);
   args[i]=p;
-  printf("send %s to stack\n",args[i]);
+  printf("send %p to stack\n",args[i]);
   }
   
   p=set_NULL(p);
@@ -81,7 +81,7 @@ void context_uload(PCB* pcb ,const char* filename,char* const argv[],char* const
   p-=int_size;
   *(uint32_t*)p=(uint32_t)args[i];
   }
-  printf("%s\n",args[0]);
+  printf("%p\n",args[0]);
   p-=int_size;
   *(uint32_t*)p=argc;
   pcb->cp->GPRx=(uintptr_t)p;
