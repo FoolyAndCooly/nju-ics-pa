506996444576251c571c1dcc66c98b250b8e3e3d
tracer-ics2022 <tracer@njuics.org>
2023-01-06 18:27:13 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:27:13 up  1:01,  1 user,  load average: 0.62, 0.80, 0.91

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -32,7 +32,7 @@ static void sh_handle_cmd(const char *cmd) {
 	while(*p++ !=' ');
 	strtok(buf," ");
 	printf("%s\n%s\n",buf,p);
-	char* argv[]={buf,NULL,NULL};
+	char* argv[]={buf,p,NULL};
 	execvp(buf,argv);
 }
 
