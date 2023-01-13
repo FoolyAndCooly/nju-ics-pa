168a2600fede9c9ac65e756e4c5656788caf8cbe
tracer-ics2022 <tracer@njuics.org>
2023-01-13 19:56:28 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:56:28 up  6:37,  1 user,  load average: 0.33, 0.52, 0.52

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -32,8 +32,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   if(phdr.p_type==PT_LOAD){
   //ramdisk_read((void*)phdr.p_vaddr,phdr.p_offset,phdr.p_filesz);
       uintptr_t va = phdr.p_vaddr; // clear low 12 bit, first page
-      uintptr_t vpage_end = (phdr.p_vaddr + phdr.p_memsz - 1) & (~0xfff); // last page start
-      int page_num = ((vpage_end - va) >> 12) + 1;
+      uintptr_t round = ROUNDUP(phdr.p_vaddr + phdr.p_memsz,PGSIZE); // last page start
+      int page_num = ((round - va) >> 12) + 1;
       uintptr_t page_ptr = (uintptr_t)new_page(page_num);
       for (int j = 0; j < page_num; ++ j) {
         map(&pcb->as, 
