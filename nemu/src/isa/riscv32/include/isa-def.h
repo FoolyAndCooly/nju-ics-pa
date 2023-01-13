18a1af8cd1cd77c0f0210633cd5dd3976377c7f4
tracer-ics2022 <tracer@njuics.org>
2023-01-13 17:34:05 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:34:05 up  4:14,  1 user,  load average: 0.27, 0.45, 0.48

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -38,6 +38,6 @@ typedef struct {
   } inst;
 } riscv32_ISADecodeInfo;
 
-#define isa_mmu_check(vaddr, len, type) ((cpu.csr.satp & 0x80000000) ? MMU_TRANSLATE : MMU_DIRECT)
+#define isa_mmu_check(vaddr, len, type) ((cpu.csr.satp >>31) ? MMU_TRANSLATE : MMU_DIRECT)
 
 #endif
