a6aa8469adccb6772ac2dc04996efcbaa7b74ab9
tracer-ics2022 <tracer@njuics.org>
2023-01-13 21:05:20 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:05:20 up  7:45,  1 user,  load average: 0.59, 0.58, 0.61

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,5 +1,5 @@
 #include <memory.h>
-
+#include <proc.h>
 static void *pf = NULL;
 
 void* new_page(size_t nr_page) {
@@ -22,13 +22,27 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
+int prot=0xe;
+if(current->max_brk == 0) current->max_brk=brk;//maybe buggy?
+if(brk > current->max_brk){
+      uintptr_t va = current->max_brk;
+      uintptr_t va_end = ROUNDUP(brk,PGSIZE)-PGSIZE;
+      int num;
+      if(va_end-va >= 0){ num = ((va_end - va) >> 12)+1;}
+      else{num=1;assert(0);}
+      void* pa = new_page(num);
+      for (int j = 0; j < num; ++ j) {
+        map(&current->as, (void*)va, (void*)pa,prot);
+        va+=PGSIZE;
+        pa+=PGSIZE;
+      }
+  }
   return 0;
 }
 
 void init_mm() {
   pf = (void *)ROUNDUP(heap.start, PGSIZE);
   Log("free physical pages starting from %p", pf);
-
 #ifdef HAS_VME
   vme_init(pg_alloc, free_page);
 #endif
