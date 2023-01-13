15d56506c7d52d002c679e7ffde8ee2e67fc5d32
tracer-ics2022 <tracer@njuics.org>
2023-01-13 21:28:00 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:28:00 up  8:08,  1 user,  load average: 0.64, 0.63, 0.67

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -23,10 +23,10 @@ void free_page(void *p) {
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
 printf("%x %x\n",current->max_brk,brk);
- /*if (current->max_brk == 0) {
+ if (current->max_brk == 0) {
     current->max_brk = brk;
     return 0;
-  }*/
+  }
   int prot = 0xe;
   for (; current->max_brk <= ROUNDUP(brk, PGSIZE); current->max_brk += PGSIZE) {
     void* pa = pg_alloc(PGSIZE);
