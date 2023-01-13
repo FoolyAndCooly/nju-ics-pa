320b06f5c5a795274d0420b7d3c5c74659ad4fda
tracer-ics2022 <tracer@njuics.org>
2023-01-13 18:11:52 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:11:52 up  4:52,  1 user,  load average: 0.62, 0.75, 0.65

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -16,15 +16,14 @@
 #include <isa.h>
 #include <memory/paddr.h>
 #include <memory/vaddr.h>
+#define PTESIZE 4
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   uint32_t vpn1 = vaddr >> 22;
-  uint32_t vpn2 = vaddr >> 12 & 0x3ff;
+  uint32_t vpn0 = vaddr >> 12 & 0x3ff;
   uint32_t offset = vaddr & 0xfff;
   paddr_t pdir = cpu.csr.satp << 12;
-  assert(pdir);
-  paddr_t ptable = paddr_read(pdir + vpn1 * 4, 4);
-  assert(ptable);
-  uint32_t pte = paddr_read(ptable + vpn2 * 4, 4);
+  paddr_t ptable = paddr_read(pdir + vpn1 * PTESIZE, PTESIZE);
+  uint32_t pte = paddr_read(ptable + vpn0 * PTESIZE, PTESIZE);
   assert(pte & 1);
   paddr_t pa = (pte & ~0xfff) | offset;
   return pa;
