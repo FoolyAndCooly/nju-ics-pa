43dcb6030e89a237adf086eafc4bba4f94611041
tracer-ics2022 <tracer@njuics.org>
2023-01-13 17:09:42 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:09:42 up  3:50,  1 user,  load average: 0.52, 0.44, 0.38

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -18,10 +18,10 @@
 #include <memory/vaddr.h>
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  uint32_t vpn1=vaddr>>22,vpn0=vaddr>>12 & 0x3ff,ppn=cpu.csr.satp>>12;
+  uint32_t vpn1=vaddr>>22,vpn0=vaddr>>12 & 0x3ff;
   uint32_t offset=vaddr & 0xfff;
-  uint32_t PGSIZE=1<<12,PTESIZE=4;
-  paddr_t a=ppn*PGSIZE;
+  uint32_t PTESIZE=4;
+  paddr_t a=cpu.csr.satp << 12;
   uint32_t pte=paddr_read(a+vpn1*PTESIZE,PTESIZE);
   uint32_t leaf_pte=paddr_read(pte+vpn0*PTESIZE,PTESIZE);
   assert(leaf_pte & 1);
