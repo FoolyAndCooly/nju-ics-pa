648c6c0ce0fa402b544a99c87617affa62da0284
tracer-ics2022 <tracer@njuics.org>
2023-01-15 16:29:59 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:29:59 up  2:40,  1 user,  load average: 0.48, 0.44, 0.41

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,10 @@ void __am_switch(Context *c);
 
 void display_context(Context *c)
 {
-printf("%d\n",(int)0xffffffbc);
+uint32_t a=0xffffffbc;
+int b;
+b=(int)a;
+printf("%d\n",b);
   for (int i = 0; i < sizeof(c->gpr) / sizeof(c->gpr[0]); i++)
   printf("gpr %d is %x\n", i, c->gpr[i]);
   printf("mcause, mstatus, mepc is %x, %x, %x\n", c->mcause, c->mstatus, c->mepc);
