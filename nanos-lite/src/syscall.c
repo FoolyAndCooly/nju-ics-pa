208cbdc86907a5b8bab9eef7ba60f342c7e0501e
tracer-ics2022 <tracer@njuics.org>
2023-01-15 20:26:39 +0800

    >  compile NEMU 211503013 刘奕诚 Linux liushao-ASUS-TUF-Gaming-F15-FX506LH-FX506LH 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:26:39 up  6:37,  1 user,  load average: 0.65, 0.58, 0.55

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -40,7 +40,6 @@ void context_uload(PCB* pcb ,const char* filename,char* const argv[],char* const
 
 void sys_yield(Context *c){
 	yield();
-	assert(0);
 	c->GPRx=0;
 #ifdef STRACE
 	printf("yield\n");
